bf1c114a0e848de9c1b5f868f1a674468a747fa7
ha <hgnguyen47347714@gmail.com>
2025-03-17 15:45:42 +0700

    test ip

--- ip.php
+++ ip.php
@@ -1,6 +1,9 @@
 <?php
-$url = "https://api.db-ip.com/v2/free/self";
-$response = file_get_contents($url);
-header('Content-Type: application/json');
-echo $response;
+// $url = "https://api.db-ip.com/v2/free/self";
+// $response = file_get_contents($url);
+// header('Content-Type: application/json');
+// echo $response;
+
+$ip = $_SERVER['REMOTE_ADDR'];
+echo "IP của bạn: " . $ip;
 ?>
